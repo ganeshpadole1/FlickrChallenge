c8c7a4b61921d9af022e968ec23a9f7e6f8e32d1
ganeshpadole <ganeshpadole55@gmail.com>
2023-07-17 14:03:12 +0530

    - added search bar

--- FlickrChallenge/FlickerSearch/Controllers/FlickrSearchViewController.swift
+++ FlickrChallenge/FlickerSearch/Controllers/FlickrSearchViewController.swift
@@ -10,19 +10,39 @@ import UIKit
 class FlickrSearchViewController: UIViewController {
 
     private var viewModel = FlickrViewModel()
+    private var searchBarController: UISearchController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "Flickr Challenge"
         view.backgroundColor = .cyan
-        
-        viewModel.search(text: "garden") {
-            print("search completed.")
-        }
-        
+       
+        addSearchBar()
         viewModelClosures()
     }
+}
 
+extension FlickrSearchViewController: UISearchControllerDelegate, UISearchBarDelegate {
+    
+    func addSearchBar() {
+        searchBarController = UISearchController(searchResultsController: nil)
+        self.navigationItem.searchController = searchBarController
+        searchBarController.delegate = self
+        searchBarController.searchBar.delegate = self
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let text = searchBar.text, text.count > 1 else {
+            return
+        }
+        
+        viewModel.search(text: text) {
+            print("user search completed.")
+        }
+        
+        searchBarController.searchBar.resignFirstResponder()
+    }
 }
 
 //MARK:- Clousers
