36bae9b9599be7cb21c67863a7e5e2218eb6b8a1
ganeshpadole <ganeshpadole55@gmail.com>
2023-07-17 17:32:05 +0530

    - changed per page record showing to 60

--- FlickrChallenge/FlickerSearch/Constants/Constants.swift
+++ FlickrChallenge/FlickerSearch/Constants/Constants.swift
@@ -9,6 +9,6 @@ import Foundation
 
 struct Constants {
     static let api_key = "a724969f016f0b8badd7e518a6c48e55"
-    static let per_page = 20
+    static let per_page = 60
     static let searchURL = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(Constants.api_key)&format=json&nojsoncallback=1&safe_search=1&per_page=\(Constants.per_page)&text=%@&page=%ld&extras=url_t"
 }
